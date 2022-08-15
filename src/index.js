690c9f7a111357118dd455a3e41d555097e77605
benthomascook28@gmail.com <benthomascook28@gmail.com>
2022-08-15 15:39:32 -0400

    added handleClick

--- src/index.js
+++ src/index.js
@@ -21,9 +21,20 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    square[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)} />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)} 
+      />
+    );
   }
 
   render() {
